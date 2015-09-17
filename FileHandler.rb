56b40fc2b63f2919a9dd5851761ca9c8fe7586f0
olalekan L. Eyiowuawi <eyiolekan@outlook.com>
2015-09-17 10:27:05 +0100

    added highscores

--- FileHandler.rb
+++ FileHandler.rb
@@ -9,6 +9,7 @@ class Filewriter
     Messages.start_up_message
   end
 def self.writer(player_name,game_time)
+  former = []
     if File.exists?"top_scores.json"
      file = File.open "top_scores.json", "r"
      if !File.zero?"top_scores.json"
@@ -20,16 +21,27 @@ def self.writer(player_name,game_time)
      file.close
      File.delete("top_scores.json")
    end
+
    obj = [
-     {"name"=>player_name,"time "=>game_time}
+     {"name"=>player_name,"time"=>game_time}
    ]
    new_obj = former + obj
+   sorted = new_obj.sort_by { |k| k["time"]}
+   if sorted.count > 10
+     sorted.pop
+   end
+   puts "     ======TOP SCORES=====     "
+
+   sorted.each do |a|
+     puts "Name: #{a["name"].upcase}  Time: #{(a["time"] / 60)}min #{(a["time"] % 60)}secs "
+   end
 
    f = File.new("top_scores.json","w")
   f.close
   File.open("top_scores.json","w") do |f|
-  f.puts JSON.pretty_generate(new_obj)
+  f.puts JSON.pretty_generate(sorted)
  end
+
 
 
 
